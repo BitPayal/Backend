e4cc31952b560e8e2e4ff7c726c556a39237f26f
BitPayal <gury20112003@gmail.com>
2025-04-20 20:46:14 +0530

    add refreshAccessToken endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
 import { loginUser,logoutUser,registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router=Router()
@@ -19,5 +20,6 @@ router.route("/register").post(
     ,registerUser)
 
     router.route("/login").post(verifyJWT,logoutUser)
+    router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
